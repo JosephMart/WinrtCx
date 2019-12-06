46de52b6179cd4c25ae7d5b6c11c7617fef6b3a9
Joseph Martinsen <josmarti@adobe.com>
2019-12-06 11:51:43 -0600

    Simple example of CX not a memebr of winrt

---
+++ WinrtApp/MainPage.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+//using namespace WinrtCx;
+
+namespace winrt::WinrtApp::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+        //this->Content = WinrtCx::BlankPage;
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
